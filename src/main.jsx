3f39a9315fc7e26f9321901d76a180877a7fc77e
mahojv <moisesmjavier@outlook.com>
2025-11-16 21:22:18 -0600

    limpiexza de repo

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
+import './index.css'
+
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material'
+import { CacheProvider } from '@emotion/react'
+import createCache from '@emotion/cache'
+
+// Cache para MUI (necesario con Vite)
+const muiCache = createCache({
+  key: 'mui',
+  prepend: true,
+})
+
+const theme = createTheme()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <CacheProvider value={muiCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </CacheProvider>
+  </StrictMode>
 )
